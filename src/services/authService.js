fc8ed5c8c5632a7d0acad59fd0b5bd5382ec56c4
JhonatanMrls <jhonatanmorales99@gmail.com>
2025-12-02 21:28:18 -0500

    feat: register conection with API

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,14 +2,12 @@ import api from './api';
 
 export default {
     async register(userData) {
-        // Esto hará un POST a: http://localhost:3000/api/auth/register
-        // Ajusta '/auth/register' según la ruta que te de tu equipo de Backend
-        const response = await api.post('/auth/register', userData);
+        const response = await api.post('/Auth/register', userData);
         return response.data;
     },
 
     async login(credentials) {
-        const response = await api.post('/auth/login', credentials);
+        const response = await api.post('/Auth/login', credentials);
         return response.data;
     }
 };
